3c492f418faa3f533c330e9541a9cc30a9e6779a
Albert Suarez <alsumo95@gmail.com>
2019-06-09 15:02:29 +0200

    :lipstick: Added white background to Project and Team pages

--- web/src/app/project/project.component.ts
+++ web/src/app/project/project.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent implements OnInit {
+export class ProjectComponent implements OnInit, AfterViewInit, OnDestroy  {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    document.querySelector('body').classList.add('white');
+  }
+
+  ngOnDestroy(): void {
+    document.querySelector('body').classList.remove('white');
   }
 
 }
--- web/src/app/team/team.component.ts
+++ web/src/app/team/team.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.scss']
 })
-export class TeamComponent implements OnInit {
+export class TeamComponent implements OnInit, AfterViewInit, OnDestroy  {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    document.querySelector('body').classList.add('white');
+  }
+
+  ngOnDestroy(): void {
+    document.querySelector('body').classList.remove('white');
   }
 
 }
